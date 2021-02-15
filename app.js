77889921dc2344b3f12a6a336fdc056fc1ee1fa9
ozgguryilmaz <ozgur_1694@hotmail.com>
2021-02-15 14:44:20 +0300

    fix

--- app.js
+++ app.js
@@ -10,14 +10,28 @@
 
 // })
 
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer = http.createServer((req, res) =>{
-    console.log('INCOMING REQUEST');
-    console.log(req.method, req.url);
+const server = (http.createServer = http.createServer((req, res) => {
+  console.log("INCOMING REQUEST");
+  console.log(req.method, req.url);
 
-    res.end('Success!')
-    
-});
- 
+  if (req.method === "POST") {
+    let body = "";
+    req.on("end", () => {
+        const userName = body.split('=')[1];
+      console.log(body);
+      res.end('<h1>' + userName + '</h1>')
+    });
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      '<form method="POST"><input type ="text" name = "username"> <button type = "submit"> Create User</button> </form>'
+    );
+  }
+}));
+
 server.listen(5000);
