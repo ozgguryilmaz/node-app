50e89ff93240c8c5d77e5a87454e70fe95aff32e
ozgguryilmaz <ozgur_1694@hotmail.com>
2021-02-15 14:23:35 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// const fs = require('fs');
+// const userName = "ozgur";
+
+// fs.writeFile("user-data.txt", 'Name: ' + userName, (err) =>{
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log('Wrote file');
+
+// })
+
+const http = require('http');
+
+http.createServer = http.createServer((req, res) =>{
+
+});
+
+server.listen(5000);
