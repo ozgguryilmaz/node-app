f5dfb06dbed118a6b95e70dc665988cb96b81654
ozgguryilmaz <ozgur_1694@hotmail.com>
2021-02-28 00:14:43 +0300

    express js

--- app.js
+++ app.js
@@ -10,28 +10,59 @@
 
 // })
 
-const http = require("http");
+// const http = require("http");
 
-const server = (http.createServer = http.createServer((req, res) => {
-  console.log("INCOMING REQUEST");
-  console.log(req.method, req.url);
+// const server = (http.createServer = http.createServer((req, res) => {
+//   console.log("INCOMING REQUEST");
+//   console.log(req.method, req.url);
 
-  if (req.method === "POST") {
-    let body = "";
-    req.on("end", () => {
-        const userName = body.split('=')[1];
-      console.log(body);
-      res.end('<h1>' + userName + '</h1>')
-    });
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-  } else {
-    res.setHeader("Content-Type", "text/html");
-    res.end(
-      '<form method="POST"><input type ="text" name = "username"> <button type = "submit"> Create User</button> </form>'
-    );
+//   if (req.method === "POST") {
+//     let body = "";
+//     req.on("end", () => {
+//         const userName = body.split('=')[1];
+//       console.log(body);
+//       res.end('<h1>' + userName + '</h1>')
+//     });
+//     req.on("data", (chunk) => {
+//       body += chunk;
+//     });
+//   } else {
+//     res.setHeader("Content-Type", "text/html");
+//     res.end(
+//       '<form method="POST"><input type ="text" name = "username"> <button type = "submit"> Create User</button> </form>'
+//     );
+//   }
+// }));
+
+// server.listen(5000);
+
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  // console.log('middleware works')
+  // next();
+  let body = "";
+  req.on("end", () => {
+    const userName = body.split("=")[1];
+    if (userName) {
+      req.body = { name: userName };
+    }
+    next();
+  });
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+});
+
+app.use((req, res, next) => {
+  if (req.body) {
+    return res.send("<h1>" + req.body.name + "</h1>");
   }
-}));
+  res.send(
+    '<form method="POST"><input type="text" name="username"> <button type="submit" type="Submit">Create User</button></form>'
+  );
+});
 
-server.listen(5000);
+app.listen(5000);
